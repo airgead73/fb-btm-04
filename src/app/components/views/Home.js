39a0582a92d38bc0bbf39880f96969a7957b7ca4
Dan Moneypenny <catullus1973@gmail.com>
2018-12-01 17:02:26 -0800

    add branch title

--- src/app/components/views/Home.js
+++ src/app/components/views/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
       return (
         <div>
           <h2>Home - {this.props.path}</h2>
-          <p>paragraph</p>
+          <p>01_Navigation Branch</p>
         </div>
       );
     }
